b1b744084390ce4c803cb10815677b20d461bace
xeffire <justinasgoberis@gmail.com>
2021-04-29 22:25:37 +0300

    add done text style

--- tasksHandler.js
+++ tasksHandler.js
@@ -33,11 +33,11 @@ function cardBuilder() {
           fragment.innerHTML += `
           <div class="card mb-1" style="border-left: 4px solid ${priorityColor[obj.priority]};">
             <div class="card-header bg-white d-flex justify-content-between">
-              <h6 id="task-name" class="text-dark"><span id="task_id">#${obj.id}</span> ${obj.name}</h6>
+              <h6 id="task-name" class="text-dark ${obj.state == 2?'fw-light italic':''}"><span id="task_id">#${obj.id}</span> ${obj.name}</h6>
               <a class="text-dark" data-id="${obj.id}" href="#edit_task" data-toggle="modal"><i class="bi bi-pencil-square" data-id="${obj.id}"></i></a>
             </div>
             <div class="card-body py-1">
-              <p id="task-description">${obj.description}</p>
+              <p id="task-description" class="${obj.state == 2?'fw-light italic':''}">${obj.description}</p>
             </div>
           </div>
           `;  
